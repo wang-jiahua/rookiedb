edf29ef76a2d5f0d24ef901b76236653fb15f5ac
jhwang <jiahua-wang@outlook.com>
2022-07-10 22:40:32 +0800

    task1 completed

--- src/main/java/edu/berkeley/cs186/database/index/LeafNode.java
+++ src/main/java/edu/berkeley/cs186/database/index/LeafNode.java
@@ -377,7 +377,22 @@ class LeafNode extends BPlusNode {
         // use the constructor that reuses an existing page instead of fetching a
         // brand new one.
 
-        return null;
+        Page page = bufferManager.fetchPage(treeContext, pageNum);
+        Buffer buf = page.getBuffer();
+
+        byte nodeType = buf.get();
+        assert(nodeType == (byte) 1);
+
+        Optional<Long> rightSibling =Optional.of(buf.getLong());
+
+        List<DataBox> keys = new ArrayList<>();
+        List<RecordId> rids = new ArrayList<>();
+        int n = buf.getInt();
+        for (int i = 0; i < n; ++i) {
+            keys.add(DataBox.fromBytes(buf, metadata.getKeySchema()));
+            rids.add(RecordId.fromBytes(buf));
+        }
+        return new LeafNode(metadata, bufferManager, page, keys, rids, rightSibling, treeContext);
     }
 
     // Builtins ////////////////////////////////////////////////////////////////
